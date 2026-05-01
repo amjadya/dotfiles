a84cd5b1a6d637191b485ec5792913984b8c325c
amjadya <amjady@student.ubc.ca>
2026-05-01 10:57:08 -0700

    add lazy.nvim to stack, configure rose-pine dawn colour scheme for nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,1 +1,5 @@
+require("config.lazy")
+
+
+
 vim.opt.number = true
---
+++ nvim/.config/nvim/lua/plugins/colourscheme.lua
@@ -0,0 +1,11 @@
+-- lua/plugins/rose-pine.lua
+return {
+	"rose-pine/neovim",
+	name = "rose-pine",
+	config = function()
+		require("rose-pine").setup({
+			variant = "dawn"
+		})
+		vim.cmd("colorscheme rose-pine")
+	end
+}
