56a02f3c81208903ff05c5ad4f7a1786a03420dd
amjadya <amjady@student.ubc.ca>
2026-05-01 00:50:05 -0700

    upgrade ff/fn functions to accept optional path arg and include hidden

--- fish/.config/fish/functions/ff.fish
+++ fish/.config/fish/functions/ff.fish
@@ -1,3 +1,23 @@
-function ff
-    set file (rg --files | fzf) && xdg-open "$file"
+function ff --description "fuzzy-find a file and open it in your default app"
+    set -l search_path .
+    if test (count $argv) -gt 0
+        set search_path $argv
+    end
+
+    for p in $search_path
+        if not test -e $p
+            echo "ff: '$p' does not exist"
+            return 1
+        end
+    end
+
+    set -l file (
+        rg --files --hidden --glob '!.git' --glob '!.cache' --glob '!node_modules' -- $search_path |
+        fzf
+    )
+
+    if test -n "$file"
+        xdg-open $file &
+        disown
+    end
 end
--- fish/.config/fish/functions/fn.fish
+++ fish/.config/fish/functions/fn.fish
@@ -1,3 +1,22 @@
-function fn
-    set file (rg --files | fzf) && nvim "$file"
+function fn --description "fuzzy-find a file and open it in nvim"
+    set -l search_path .
+    if test (count $argv) -gt 0
+        set search_path $argv
+    end
+
+    for p in $search_path
+        if not test -e $p
+            echo "fn: '$p' does not exist"
+            return 1
+        end
+    end
+
+    set -l file (
+        rg --files --hidden --glob '!.git' --glob '!.cache' --glob '!node_modules' -- $search_path |
+        fzf
+    )
+
+    if test -n "$file"
+        nvim $file
+    end
 end
