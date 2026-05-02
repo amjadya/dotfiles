2692f5bffe417cfc9ffcb9b835d8406314764597
amjadya <amjady@student.ubc.ca>
2026-05-01 23:36:46 -0700

    default tabs to 4 spaces, fix silly indenting in previous push

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,5 +1,8 @@
 require("config.lazy")
 
-
-
 vim.opt.number = true
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = -1
--- nvim/.config/nvim/lua/plugins/colourscheme.lua
+++ nvim/.config/nvim/lua/plugins/colourscheme.lua
@@ -9,16 +9,17 @@
 -- 		vim.cmd("colorscheme rose-pine")
 -- 	end
 -- }
-    return {
-      'sainnhe/gruvbox-material',
-      lazy = false,
-      priority = 1000,
-      config = function()
+
+return {
+    'sainnhe/gruvbox-material',
+    lazy = false,
+    priority = 1000,
+    config = function()
         -- Optionally configure and load the colorscheme
         -- directly inside the plugin declaration.
-	vim.o.background = 'light'
-	vim.g.gruvbox_material_foreground = 'original'
+        vim.o.background = 'light'
+        vim.g.gruvbox_material_foreground = 'original'
         vim.g.gruvbox_material_enable_italic = true
         vim.cmd.colorscheme('gruvbox-material')
-      end
-    }
+    end
+}
