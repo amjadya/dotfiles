3251b0e8d44eb429959855438e554b4389b5a464
amjadya <amjady@student.ubc.ca>
2026-05-01 23:08:39 -0700

    add ghostty to the dotfiles, change font to jetbrains mono nerd, gruvbox light mode is  so fire

--- nvim/.config/nvim/lua/plugins/colourscheme.lua
+++ nvim/.config/nvim/lua/plugins/colourscheme.lua
@@ -1,11 +1,24 @@
 -- lua/plugins/rose-pine.lua
-return {
-	"rose-pine/neovim",
-	name = "rose-pine",
-	config = function()
-		require("rose-pine").setup({
-			variant = "dawn"
-		})
-		vim.cmd("colorscheme rose-pine")
-	end
-}
+-- return {
+-- 	"rose-pine/neovim",
+-- 	name = "rose-pine",
+-- 	config = function()
+-- 		require("rose-pine").setup({
+-- 			variant = "moon"
+-- 		})
+-- 		vim.cmd("colorscheme rose-pine")
+-- 	end
+-- }
+    return {
+      'sainnhe/gruvbox-material',
+      lazy = false,
+      priority = 1000,
+      config = function()
+        -- Optionally configure and load the colorscheme
+        -- directly inside the plugin declaration.
+	vim.o.background = 'light'
+	vim.g.gruvbox_material_foreground = 'original'
+        vim.g.gruvbox_material_enable_italic = true
+        vim.cmd.colorscheme('gruvbox-material')
+      end
+    }
