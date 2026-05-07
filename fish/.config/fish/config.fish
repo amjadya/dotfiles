863449bd18ae02f696e02eead87ab509313c9198
amjadya <amjady@student.ubc.ca>
2026-05-07 11:53:49 -0700

    feat: introduce LSPs (finally) and improve alt+c fzf command with maximum depth

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -10,7 +10,7 @@ fzf --fish | source
 
 set -x FZF_IGNORE_GLOBS --glob '!.git' --glob '!.cache' --glob '!node_modules'
 set -x FZF_IGNORE_EXCLUDES --exclude '.git' --exclude '.cache' --exclude 'node_modules'
-set -x FZF_ALT_C_COMMAND "fd -H --type d --follow $FZF_IGNORE_EXCLUDES --search-path '/home/meej'"
+set -x FZF_ALT_C_COMMAND "fd -H --max-depth 5 --type d --follow $FZF_IGNORE_EXCLUDES --search-path '/home/meej'"
 set -x FZF_ALT_C_OPTS '--no-height'
 
 # Git abbreviations (expand visibly when you press space)
