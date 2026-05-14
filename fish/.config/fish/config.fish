3893ba1801c0fcf045b3b76022db6a7f998e0091
amjadya <amjady@student.ubc.ca>
2026-05-13 23:33:10 -0700

    feat: init.lua changes + sourcing my compsci course + formatting

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,28 +1,30 @@
 if status is-interactive
-# Commands to run in interactive sessions can go here
+    # Commands to run in interactive sessions can go here
 
-function fish_greeting
-	echo "yo"
-end
+    function fish_greeting
+        echo yo
+    end
 
-starship init fish | source
-fzf --fish | source
+    starship init fish | source
+    fzf --fish | source
 
-set -x FZF_IGNORE_GLOBS --glob '!.git' --glob '!.cache' --glob '!node_modules'
-set -x FZF_IGNORE_EXCLUDES --exclude '.git' --exclude '.cache' --exclude 'node_modules'
-set -x FZF_ALT_C_COMMAND "fd -H --max-depth 5 --type d --follow $FZF_IGNORE_EXCLUDES --search-path '/home/meej'"
-set -x FZF_ALT_C_OPTS '--no-height'
+    set -x FZF_IGNORE_GLOBS --glob '!.git' --glob '!.cache' --glob '!node_modules'
+    set -x FZF_IGNORE_EXCLUDES --exclude '.git' --exclude '.cache' --exclude node_modules
+    set -x FZF_ALT_C_COMMAND "fd -H --max-depth 5 --type d --follow $FZF_IGNORE_EXCLUDES --search-path '/home/meej'"
+    set -x FZF_ALT_C_OPTS --no-height
 
-# Git abbreviations (expand visibly when you press space)
-abbr -a g git
-abbr -a gs git status
-abbr -a ga git add
-abbr -a gaa git add .
-abbr -a gc git commit -m
-abbr -a gp git push
-abbr -a gpl git pull
-abbr -a gd git diff
-abbr -a gco git checkout
-abbr -a gb git branch
+    # Git abbreviations (expand visibly when you press space)
+    abbr -a g git
+    abbr -a gs git status
+    abbr -a ga git add
+    abbr -a gaa git add .
+    abbr -a gc git commit -m
+    abbr -a gp git push
+    abbr -a gpl git pull
+    abbr -a gd git diff
+    abbr -a gco git checkout
+    abbr -a gb git branch
+
+    abbr -a cpsc330 source ~/.venvs/cpsc330/bin/activate.fish
 
 end
--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,8 +3,19 @@ vim.filetype.add({ extension = { mdx = "markdown" } })
 require("config.lazy")
 
 vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.wrap = false
+vim.opt.scrolloff = 10
+vim.opt.sidescrolloff = 8
+
+vim.opt.signcolumn = "yes"
 
 vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = -1
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.clipboard = "unnamedplus"
